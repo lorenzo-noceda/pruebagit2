4d4271b2cbf45148990c42190c2d60f0f5003588
Lorenzo Facundo Noceda <lorenzonoceda7@gmail.com>
2023-11-07 16:19:21 -0300

    dsf

--- src/ar/edu/unlam/pb2/eva03/Batalla.java
+++ src/ar/edu/unlam/pb2/eva03/Batalla.java
@@ -12,7 +12,7 @@ import ar.edu.unlam.pb2.eva03.interfaces.Volador;
 public class Batalla {
 	private Double latitud;
 	private Double longitud;
-	private TipoDeBatalla tipo;
+	private TipoDeBatalla tiddpo;
 	Set<Vehiculo> vehiculosEnLaBatalla;
 	
 }
